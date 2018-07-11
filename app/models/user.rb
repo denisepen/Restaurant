34cc18d22cc88c9f6424e12c0a06e1e805fba786
Denise Pendleton-Lipinski <denisependletonlipinski@Denises-MacBook-Pro.local>
2018-07-10 22:11:07 -0400

    added meal partial for new and edit templates

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   has_many :meals, through: :orders
 
   validates_uniqueness_of :email
-  validates_presence_of :first_name, :last_name, :email
-  validates :first_name, :last_name, length: { minimum: 2 }
-  validates :password, length: { in: 4..20 }
+  validates_presence_of :first_name, :last_name, :email, :password_confirmation
+  validates :first_name, :last_name,  length: { minimum: 3 }
+  validates :password,  length: { in: 6..20 }
 
   end
