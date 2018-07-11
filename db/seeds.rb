c1f4e7090c314c54f46053515078a8f499792ed6
Denise Pendleton-Lipinski <denisependletonlipinski@Denises-MacBook-Pro.local>
2018-07-10 22:33:19 -0400

    updated seed file - added meals

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.delete_all
+Meal.delete_all
 
 User.create!(first_name:  "Denise",
               last_name:  "Owner",
@@ -26,7 +27,56 @@ User.create!(first_name:  "Denise",
                password_confirmation: password)
 end
 
-# 20.times do |n|
-#   desc = Faker::Food.description
-#   Meal.create!(description: desc)
-# end
+Meal.create(
+  name: "Beef and Broccoli",
+  price: 7.95,
+  description: "Beef and broccoli stir fried in
+  a delicious garlic sauce",
+  calorie_count: 550,
+  category: "Main"
+)
+
+Meal.create(
+  name: "Chicken and Broccoli",
+  price: 8.95,
+  description: "Chicken and broccoli stir fried in
+  a delicious garlic sauce",
+  calorie_count: 550,
+  category: "Main"
+)
+
+Meal.create(
+  name: "Mashed Potatoes",
+  price: 5.95,
+  description: "Organically grown red potatoes pureed
+    with garlic and sour cream and drizzled in a delicious
+    brown gravy",
+  calorie_count: 775,
+  category: "Side"
+)
+
+Meal.create(
+  name: "Steamed Vegetables",
+  price: 5.95,
+  description: "Organically grown vegetable medley
+  steamed to perfection",
+  calorie_count: 250,
+  category: "Side"
+)
+
+Meal.create(
+  name: "Red Wine",
+  price: 10.95,
+  description: "Delicious red wine from the Bordeaux Region",
+  calorie_count: 775,
+  category: "Drink"
+)
+
+Meal.create(
+  name: "Chocolate Lava Cake",
+  price: 8.00,
+  description: "Delicious chocolate cake with warm and
+    decadent center",
+  calorie_count: 600,
+  category: "Dessert"
+)
